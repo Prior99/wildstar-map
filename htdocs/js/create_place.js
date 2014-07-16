d3db195224a06e2dc78f7fe904909821674d8df6
Prior <fgnodtke@cronosx.de>
2014-07-16 08:53:32 +0200

    Improved adding places

--- htdocs/js/create_place.js
+++ htdocs/js/create_place.js
@@ -4,35 +4,57 @@ function createPlace(x, y, map) {
 	div.append("<h1>Create New Place</h1>");
 	Connection.getCategories(function(categories) {
 		var table = $("<table></table>").appendTo(div);
-		var select = $("<select size='1'></select>")
-			.appendTo($("<td></td>")
-				.appendTo($("<tr></tr>")
+		var select = $("<td></td>")
+				.appendTo($("<tr style='height: 50px;'></tr>")
 					.append("<td>Category</td>")
-					.appendTo(table)));
-		for(var i in categories) {
-			var cat = categories[i];
-			console.log(cat);
-			select.append($("<option value='" + cat.id + "'>" + cat.name + "</option>"));
-		}
+					.appendTo(table));
+		var selected = undefined;
+		var selectedID = undefined;
 		var name = $("<input type='text' />")
 			.appendTo($("<td></td>")
 				.appendTo($("<tr></tr>")
 					.append("<td>Name</td>")
-					.appendTo(table)));
+					.appendTo(table)))
+			.keyup(function() {
+				checkOkay();
+			});
 		var desc = $("<textarea></textarea>")
 			.appendTo($("<td></td>")
 				.appendTo($("<tr></tr>")
 					.append("<td>Description</td>")
 					.appendTo(table)));
-		$("<button>Okay</button>")
+		var button = $("<button>Okay</button>")
 			.appendTo($("<td></td>")
 				.appendTo($("<tr></tr>")
 					.append("<td>Submit</td>")
 					.appendTo(table)))
 			.click(function() {
-				Connection.submitPlace(x, y, select.val(), name.val(), desc.val(), map, function() {
+				Connection.submitPlace(x, y, selectedID, name.val(), desc.val(), map, function() {
 					div.remove();
 				});
-			});
+			}).attr("disabled", "disabled");
+		function checkOkay() {
+			if(selectedID && name.val().length > 2) {
+				button.removeAttr("disabled");
+			}
+			else {
+				button.attr("disabled", "disabled");
+			}
+		}
+		for(var i in categories) {
+			var cat = categories[i];
+			console.log(cat);
+			var d = $("<div class='select'>" + cat.name + "</div>").appendTo(select);
+			d.prepend("<img width='24' height='24' src='icons/" + cat.icon + "' />");
+			(function(d, id) {
+				d.click(function(){
+					if(selected) selected.removeClass("selected");
+					selected = d;
+					selected.addClass("selected");
+					selectedID = id;
+					checkOkay();
+				});
+			})(d, cat.id);
+		}
 	});
 }
