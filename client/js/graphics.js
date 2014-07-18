3fef04607e1684652ca25c18a80d1c32d49489b1
Frederick Gnodtke <fgnodtke@cronosx.de>
2014-07-18 07:38:50 +0200

    Locations will be opaque if under a votescore of 2. On zoomfactor less than 6 no titles will be display above locations

--- client/js/graphics.js
+++ client/js/graphics.js
@@ -234,22 +234,24 @@ Graphics.prototype = {
                      * Will draw the place with its icon and text
                      */
                     function draw(img, coord) {
-                        if(place.score < 0) self.ctx.globalAlpha = 0.5;
+                        if(place.score < 2) self.ctx.globalAlpha = 0.5;
                         self.ctx.drawImage(img, //First, draw the icon
                             coord.x - self.iconsize/2,
                             coord.y - self.iconsize/2,
                             self.iconsize, self.iconsize
                         );
-                        self.ctx.textAlign = "center"; //center text over icon
-                        self.ctx.font = "15px Verdana"; //bigger font
-                        self.ctx.strokeStyle = "black;" //black border for improved visibility
-                        self.ctx.fillStyle = "white"; //White fontcolor
-                        self.ctx.strokeText(place.name, coord.x, coord.y - self.iconsize/2 - 5 - 14); //Draw the name
-                        self.ctx.fillText(place.name, coord.x, coord.y - self.iconsize/2 - 5 - 14);
-                        self.ctx.font = "11px Verdana"; //Smaller font
-                        self.ctx.strokeText(place.category, coord.x, coord.y - self.iconsize/2 - 5); //Draw the category
-                        self.ctx.fillText(place.category, coord.x, coord.y - self.iconsize/2 - 5);
-                        if(place.score < 0) self.ctx.globalAlpha = 1;
+                        if(self.factor < 6) {
+                            self.ctx.textAlign = "center"; //center text over icon
+                            self.ctx.font = "15px Verdana"; //bigger font
+                            self.ctx.strokeStyle = "black;" //black border for improved visibility
+                            self.ctx.fillStyle = "white"; //White fontcolor
+                            self.ctx.strokeText(place.name, coord.x, coord.y - self.iconsize/2 - 5 - 14); //Draw the name
+                            self.ctx.fillText(place.name, coord.x, coord.y - self.iconsize/2 - 5 - 14);
+                            self.ctx.font = "11px Verdana"; //Smaller font
+                            self.ctx.strokeText(place.category, coord.x, coord.y - self.iconsize/2 - 5); //Draw the category
+                            self.ctx.fillText(place.category, coord.x, coord.y - self.iconsize/2 - 5);
+                        }
+                        if(place.score < 2) self.ctx.globalAlpha = 1;
                     }
                     var coord = self.transformRealToRenderCoordinates(place.x, place.y); //Get rendercoordinates
                     if(icon = self.icons[place.icon]) { //If icon already cached, nothing to be done, just draw the damn thing
